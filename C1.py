599bc488527760576ef1a47cec78d8d0f3af4b61
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2021-06-01 14:04:54 +0800

    Update C1.py

--- C1.py
+++ C1.py
@@ -1,14 +1,25 @@
-import socket
-ip=socket.gethostbyname(socket.gethostname())
+import socket,threading
+
+ip = socket.gethostbyname(socket.gethostname())
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(3)
-    s.connect(('192.168.116.100',1001))
-    for i in range(1,2):
-        s.send("{}EOF  {}      what are you doing?".format(ip,i).encode()) 
-        time.sleep(0.05)
-except Exception as f :
-    print(f,"连接失败")
+    # s.settimeout(3)
+    s.connect(('192.168.116.100', 1001))
+
+
+    def shou():
+        while True:
+            a = s.recv(1024).decode()
+            if a:
+                print("\n{}\n".format(a))
+
+
+    threading.Thread(target=shou).start()
+    while True:
+        a = input()
+        s.send('{}: {}'.format(ip, a).encode())
+except Exception as f:
+    print(f, "连接失败")
 else:
-    
+
     print('发送成功')
