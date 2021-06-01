c7fb87fba3c1296c7420184d7aad1ba3a23635e9
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2021-06-01 12:44:50 +0800

    Update S1.py

--- S1.py
+++ S1.py
@@ -1,5 +1,5 @@
-import socket,threading
-addr = ('192.168.116.100',1001) #定义socket绑定的地址，ip地址为本地，端口为1001
+import socket,threading,time
+addr = (socket.gethostbyname(socket.gethostname()),1001) #定义socket绑定的地址，ip地址为本地，端口为1001
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     #创建一个socket对象
 s.bind(addr)            #绑定地址
 s.listen(100)
@@ -9,16 +9,10 @@ while True:
     cl, addr = s.accept() #接受客户端的连接请求，cl为此链接创建的一个新的scoket对象，addr客户端地址
     def start():
      while True:
+        time.sleep(0.05)
         msg = cl.recv(1024)
         value=msg.decode()
-        if value == 'go':
-            print("go")
-        elif value == 'ba':
-            print("ba")
-        elif value == 'le':
-            print("le")
-        elif value == 'ri':
-            print("ri")
-        else:
-            print(value)
+        if value:
+            print('我的IP地址',value.split('EOF')[0],'我发送值',value.split('EOF')[1])
+        #break
     threading.Thread(target=start).start()
