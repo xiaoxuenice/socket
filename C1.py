045ca617350c5e06d64cab6571324d26cb4b3d8b
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2021-06-01 12:43:31 +0800

    Update C1.py

--- C1.py
+++ C1.py
@@ -1,4 +1,14 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.116.100",1001))
-s.send("go".encode())
+ip=socket.gethostbyname(socket.gethostname())
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    s.connect(('192.168.116.100',1001))
+    for i in range(1,2):
+        s.send("{}EOF  {}      what are you doing?".format(ip,i).encode()) 
+        time.sleep(0.05)
+except Exception as f :
+    print(f,"连接失败")
+else:
+    
+    print('发送成功')
